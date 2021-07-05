d625d0e61365a103d6602ea4e19ca0594c6e3eaa
Motoki-Kagami <73170508+Motoki-Kagami@users.noreply.github.com>
2021-07-05 17:18:59 +0900

    Add files via upload

---
+++ Ricedice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+  int d1,d2;
+  srand(time(NULL));
+  
+  d1=rand()%6+1;
+  d2=rand()%6+1;
+
+  printf("Rolling the dice...\n");
+  printf("Dice1: %d \n",d1);
+  printf("Dice2: %d \n",d2);
+  printf("Total value: %d \n",d1+d2);
+
+  return 0;
+}
