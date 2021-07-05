f7542f167a639d39b4cb2b60ab587dabf69ba746
Motoki-Kagami <s1280200@u-aizu.ac.jp>
2021-07-05 17:55:51 +0900

    Greetings added

--- Ricedice.c
+++ Ricedice.c
@@ -6,10 +6,14 @@ int main(){
 
   int d1,d2;
   srand(time(NULL));
-  
+
+  char name[20];
   d1=rand()%6+1;
   d2=rand()%6+1;
 
+  printf("What is your name?\n>");
+  scanf(" %s",name);
+  printf("Hello, %s!\n",name); 
   printf("Rolling the dice...\n");
   printf("Dice1: %d \n",d1);
   printf("Dice2: %d \n",d2);
